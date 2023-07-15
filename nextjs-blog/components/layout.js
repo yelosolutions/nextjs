800f6a81c15f5dc8a891ea9746aa4766c46c8e1c
jay <ronokamau@gmail.com>
2023-07-16 00:10:46 +0300

    Blog site initial setup

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,21 +1,71 @@
 import styles from './layout.module.css';
-import Header from 'next/head';
-import utils from '../styles/utils.module.css'
-import Image from 'next/head'
+import Head from 'next/head';
+import utilStyles from '../styles/utils.module.css'; 
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default function Layout({children}) {
+const name = 'Jay';
+export const siteTitle = 'Creating an blog app with Next.js';
 
-    const name = 'Jay';
 
-    return <>
-    {home? (
-        <Header>
-            <Image
-                sr
-            />
-        </Header>
-    ) : (
+export default function Layout({children, home}) {
 
-    )}
-    </>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="description"
+                    content="Learn how to build a personal website using Next.js"
+                />
+                <meta
+                    property="og:image"
+                    content={`https://og-image.vercel.app/${encodeURI(
+                    siteTitle,
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                />
+                <meta name="og:title" content={siteTitle} />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+            <header className={styles.header}>
+            {home ? (
+                <>
+                <Image
+                    priority
+                    src="/images/profile.jpg"
+                    className={utilStyles.borderCircle}
+                    height={144}
+                    width={144}
+                    alt=""
+                />
+                <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            </>
+            ) : (
+            <>
+                <Link href="/">
+                <Image
+                    priority
+                    src="/images/profile.jpg"
+                    className={utilStyles.borderCircle}
+                    height={108}
+                    width={108}
+                    alt=""
+                />
+                </Link>
+                <h2 className={utilStyles.headingLg}>
+                <Link href="/" className={utilStyles.colorInherit}>
+                    {name}
+                </Link>
+                </h2>
+            </>
+            )}
+            </header>
+            <main>{children}</main>
+            {!home && (
+                <div className={styles.backToHome}>
+                <Link href="/">← Back to home</Link>
+                </div>
+            )}
+        </div>
+    );
 }
--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,20 +1,22 @@
 import Head from 'next/head';
+import Layout, {siteTitle} from '../components/layout';
 import styles from '../styles/Home.module.css';
+import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Find <Link href='/posts/first-post'> First Post</Link>
-        </h1>
-      </main>
-    </div>
+      <section className={utilStyles.headingMd}>
+        <p>[I am a web developer]</p>
+        <p>
+          (This is a sample website - I’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
   )
 }
