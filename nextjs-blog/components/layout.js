388cc4c199fbf8f99ed2e86c2958a481df884055
jay <ronokamau@gmail.com>
2023-07-15 23:52:37 +0300

    Layout and css modules

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,5 +1,21 @@
-import styles from './layout.module.css'
+import styles from './layout.module.css';
+import Header from 'next/head';
+import utils from '../styles/utils.module.css'
+import Image from 'next/head'
 
 export default function Layout({children}) {
-    return <div className={styles.container}>{children}</div>
+
+    const name = 'Jay';
+
+    return <>
+    {home? (
+        <Header>
+            <Image
+                sr
+            />
+        </Header>
+    ) : (
+
+    )}
+    </>
 }
